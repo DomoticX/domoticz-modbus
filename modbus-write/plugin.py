5b58aa52dcaa258e8dc05f90f4c7e930af8353a5
Sebastiaan Ebeltjes <info@domoticx.nl>
2018-01-08 20:15:37 +0100

    0.2.0 Beta2

--- modbus-write/plugin.py
+++ modbus-write/plugin.py
@@ -1,14 +1,23 @@
 # ModbusRTU - SWITCH (USB RS485-Serial) Plugin for Domoticz
 #
 # Author: Sebastiaan Ebeltjes / domoticx.nl
+# Serial HW: USB RS485-Serial Stick
 #
-# Install modbus library: 
+# Dependancies:
+# - PYMODBUS: Install for python 3 with: pip3 install -U pymodbus3
 #
 """
-<plugin key="ModbusRTU" name="ModbusRTU - Universal WRITE (USB RS485-Serial)" author="S. Ebeltjes / domoticx.nl" version="1.0.0" externallink="" wikilink="">
+<plugin key="ModbusRTU" name="Modbus - Universal WRITE" author="S. Ebeltjes / domoticx.nl" version="0.2.0" externallink="" wikilink="">
     <params>
+        <param field="Mode1" label="Method" width="60px" required="true">
+            <options>
+                <option label="RTU" value="rtu" default="true"/>
+                <option label="ASCII" value="ascii"/>
+                <option label="TCP" value="tcp"/>
+            </options>
+        </param>
         <param field="SerialPort" label="Serial Port" width="150px" required="true"/>
-        <param field="Mode1" label="BaudRate" width="60px" required="true">
+        <param field="Mode2" label="BaudRate" width="60px" required="true">
             <options>
                 <option label="1200" value="1200"/>
                 <option label="2400" value="2400"/>
@@ -21,26 +30,23 @@
                 <option label="115200" value="115200"/>
             </options>
         </param>
-        <param field="Mode2" label="StopBits" width="60px" required="true">
+        <param field="Mode3" label="PortSettings" width="260px" required="true">
             <options>
-                <option label="1" value="1" default="true"/>
-                <option label="2" value="2"/>
+                <option label="StopBits 1 / ByteSize 7 / Parity: None" value="S1B7PN"/>
+                <option label="StopBits 1 / ByteSize 7 / Parity: Even" value="S1B7PE"/>
+                <option label="StopBits 1 / ByteSize 7 / Parity: Odd" value="S1B7PO"/>
+                <option label="StopBits 1 / ByteSize 8 / Parity: None" value="S1B8PN" default="true"/>
+                <option label="StopBits 1 / ByteSize 8 / Parity: Even" value="S1B8PE"/>
+                <option label="StopBits 1 / ByteSize 8 / Parity: Odd" value="S1B8PO"/>
+                <option label="StopBits 2 / ByteSize 7 / Parity: None" value="S2B7PN"/>
+                <option label="StopBits 2 / ByteSize 7 / Parity: Even" value="S2B7PE"/>
+                <option label="StopBits 2 / ByteSize 7 / Parity: Odd" value="S2B7PO"/>
+                <option label="StopBits 2 / ByteSize 8 / Parity: None" value="S2B8PN"/>
+                <option label="StopBits 2 / ByteSize 8 / Parity: Even" value="S2B8PE"/>
+                <option label="StopBits 2 / ByteSize 8 / Parity: Odd" value="S2B8PO"/>
             </options>
         </param>
-        <param field="Mode3" label="ByteSize" width="60px" required="true">
-            <options>
-                <option label="7" value="7"/>
-                <option label="8" value="8" default="true"/>
-            </options>
-        </param>
-        <param field="Mode4" label="Parity" width="60px" required="true">
-            <options>
-                <option label="None" value="N" default="true"/>
-                <option label="Even" value="E"/>
-                <option label="Odd" value="O"/>
-            </options>
-        </param>
-        <param field="Address" label="Device adress" width="75px" required="true"/>
+        <param field="Mode4" label="Device address" width="75px" required="true"/>
         <param field="Username" label="Functie" width="280px" required="true">
             <options>
                 <option label="Write Single Coil (Function 5)" value="5"/>
@@ -64,18 +70,18 @@ sys.path.append('/usr/local/lib/python3.5/dist-packages/pyserial-3.3-py3.5.egg')
 sys.path.append('/usr/local/lib/python3.5/dist-packages')
 
 from pymodbus3.client.sync import ModbusSerialClient
+from pymodbus3.client.sync import ModbusTcpClient
 
 class BasePlugin:
     enabled = False
-    global data
     def __init__(self):
         return
 
     def onStart(self):
-       #Domoticz.Log("onStart called")
-       if (len(Devices) == 0): Domoticz.Device(Name="ModbusRTU", Unit=1, TypeName="Switch", Image=0, Used=1).Create() #Used=1 to add a switch immediatly!
+       # Domoticz.Log("onStart called")
+       if (len(Devices) == 0): Domoticz.Device(Name="ModbusDEV", Unit=1, TypeName="Switch", Image=0, Used=1).Create() #Used=1 to add a switch immediatly!
        DumpConfigToLog()
-       Domoticz.Log("ModbusRTU - Universal WRITE (USB RS485-Serial) loaded.")
+       Domoticz.Log("Modbus - Universal WRITE loaded.")
        return
 
     def onStop(self):
@@ -89,25 +95,76 @@ class BasePlugin:
         Domoticz.Log("onMessage called")
 
     def onCommand(self, Unit, Command, Level, Hue):
-        #Domoticz.Log("onCommand called")
+        # Domoticz.Log("onCommand called")
         Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
-        Domoticz.Log("MODBUS DEBUG HW - Port="+Parameters["SerialPort"]+" BaudRate="+Parameters["Mode1"]+" StopBits="+Parameters["Mode2"]+" ByteSize="+Parameters["Mode3"]+" Parity="+Parameters["Mode4"]) # DEBUG LINE
-        Domoticz.Log("MODBUS DEBUG CMD - Address="+Parameters["Address"]+" Register="+Parameters["Port"]+" Function="+Parameters["Username"]+" PayLoadON="+Parameters["Mode5"]+" PayLoadOFF="+Parameters["Mode6"]) # DEBUG LINE
 
-        #Which payload to execute?
+        # Wich port settings to use? (todo improvement: could be array table or something...)
+        if (Parameters["Mode3"] == "S1B7PN"):
+           StopBits=1
+           ByteSize=7
+           Parity="N"
+        if (Parameters["Mode3"] == "S1B7PE"):
+           StopBits=1
+           ByteSize=7
+           Parity="E"
+        if (Parameters["Mode3"] == "S1B7PO"):
+           StopBits=1
+           ByteSize=7
+           Parity="O"
+        if (Parameters["Mode3"] == "S1B8PN"):
+           StopBits=1
+           ByteSize=8
+           Parity="N"
+        if (Parameters["Mode3"] == "S1B8PE"):
+           StopBits=1
+           ByteSize=8
+           Parity="E"
+        if (Parameters["Mode3"] == "S1B8PO"):
+           StopBits=1
+           ByteSize=8
+           Parity="O"
+        if (Parameters["Mode3"] == "S2B7PN"):
+           StopBits=2
+           ByteSize=7
+           Parity="N"
+        if (Parameters["Mode3"] == "S2B7PE"):
+           StopBits=2
+           ByteSize=7
+           Parity="E"
+        if (Parameters["Mode3"] == "S2B7PO"):
+           StopBits=2
+           ByteSize=7
+           Parity="O"
+        if (Parameters["Mode3"] == "S2B8PN"):
+           StopBits=2
+           ByteSize=8
+           Parity="N"
+        if (Parameters["Mode3"] == "S2B8PE"):
+           StopBits=2
+           ByteSize=8
+           Parity="E"
+        if (Parameters["Mode3"] == "S2B8PO"):
+           StopBits=2
+           ByteSize=8
+           Parity="O"
+
+        # Which payload to execute?
         if (str(Command) == "On"): payload = Parameters["Mode5"]
         if (str(Command) == "Off"): payload = Parameters["Mode6"]
 
+        Domoticz.Log("MODBUS DEBUG HW-USB SERIAL - Port="+Parameters["SerialPort"]+" BaudRate="+Parameters["Mode2"]+" StopBits="+str(StopBits)+" ByteSize="+str(ByteSize)+" Parity="+Parity) # DEBUG LINE
+        Domoticz.Log("MODBUS DEBUG CMD - Method="+Parameters["Mode1"]+" Address="+Parameters["Mode4"]+" Register="+Parameters["Port"]+" Function="+Parameters["Username"]+" PayLoadON="+Parameters["Mode5"]+" PayLoadOFF="+Parameters["Mode6"]) # DEBUG LINE
+
         try:
-          client = ModbusSerialClient(method="rtu", port=Parameters["SerialPort"], stopbits=int(Parameters["Mode2"]), bytesize=int(Parameters["Mode3"]), parity=Parameters["Mode4"], baudrate=int(Parameters["Mode1"]), timeout=1, retries=2)
+          client = ModbusSerialClient(method=Parameters["Mode1"], port=Parameters["SerialPort"], stopbits=StopBits, bytesize=ByteSize, parity=Parity, baudrate=int(Parameters["Mode2"]), timeout=1, retries=2)
 
-          #Which function to execute?
-          if (Parameters["Username"] == "5"): data = client.write_coil(int(Parameters["Port"]), int(payload, 16), unit=int(Parameters["Address"]))
-          if (Parameters["Username"] == "6"): data = client.write_register(int(Parameters["Port"]), int(payload, 16), unit=int(Parameters["Address"]))
-          if (Parameters["Username"] == "15"): data = client.write_coils(int(Parameters["Port"]), int(payload, 16), unit=int(Parameters["Address"]))
-          if (Parameters["Username"] == "16"): data = client.write_registers(int(Parameters["Port"]), int(payload, 16), unit=int(Parameters["Address"]))
+          # Which function to execute?
+          if (Parameters["Username"] == "5"): data = client.write_coil(int(Parameters["Port"]), int(payload, 16), unit=int(Parameters["Mode4"]))
+          if (Parameters["Username"] == "6"): data = client.write_register(int(Parameters["Port"]), int(payload, 16), unit=int(Parameters["Mode4"]))
+          if (Parameters["Username"] == "15"): data = client.write_coils(int(Parameters["Port"]), int(payload, 16), unit=int(Parameters["Mode4"]))
+          if (Parameters["Username"] == "16"): data = client.write_registers(int(Parameters["Port"]), int(payload, 16), unit=int(Parameters["Mode4"]))
 
-	  #Domoticz.Log(data)
+	  # Domoticz.Log(data) #TODO DEBUG MODBUS OUTPUT
           client.close()
           if (str(Command) == "On"): Devices[1].Update(1, "1") #Update device to ON in Domoticz
           if (str(Command) == "Off"): Devices[1].Update(0, "0") #Update device to OFF in Domoticz
@@ -123,7 +180,7 @@ class BasePlugin:
         Domoticz.Log("onDisconnect called")
 
     def onHeartbeat(self):
-        #Domoticz.Log("onHeartbeat called")
+        # Domoticz.Log("onHeartbeat called")
         return
 
     def UpdateDevice(Unit, nValue, sValue):
